60a35909c303b41f0c64d67e46e54b8bf0fabe28
unknown <kumardeepsingh@student.kpu.ca>
2025-11-06 20:46:14 -0800

    Initial Commit

---
+++ src/main/java/com/kumar/tasks/domain/entities/TaskPriority.java
@@ -0,0 +1,5 @@
+package com.kumar.tasks.domain.entities;
+
+public enum TaskPriority {
+    HIGH, MEDIUM, LOW
+}
---
+++ src/main/java/com/kumar/tasks/domain/entities/TaskStatus.java
@@ -0,0 +1,5 @@
+package com.kumar.tasks.domain.entities;
+
+public enum TaskStatus {
+    OPEN, CLOSED
+}
