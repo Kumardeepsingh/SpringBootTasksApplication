a1d63d70fae670fb65604d5b513ed361847bc5cd
unknown <kumardeepsingh@student.kpu.ca>
2025-11-09 17:41:03 -0800

    Added create task endpoint

--- src/main/java/com/kumar/tasks/controller/TasksController.java
+++ src/main/java/com/kumar/tasks/controller/TasksController.java
@@ -1,12 +1,10 @@
 package com.kumar.tasks.controller;
 
 import com.kumar.tasks.domain.dto.TaskDto;
+import com.kumar.tasks.domain.entities.Task;
 import com.kumar.tasks.mappers.TaskMapper;
 import com.kumar.tasks.services.TaskService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -30,5 +28,16 @@ public class TasksController {
                 .stream()
                 .map(taskMapper::toDto)
                 .toList();
+    }
+
+    @PostMapping
+    public TaskDto createTask(
+            @PathVariable("task_list_id") UUID taskListId,
+            @RequestBody TaskDto taskDto){
+        Task createdTask = taskService.createTask(
+                taskListId,
+                taskMapper.fromDto(taskDto)
+        );
+        return taskMapper.toDto(createdTask);
     }
 }
--- src/main/java/com/kumar/tasks/services/impl/TaskServiceImpl.java
+++ src/main/java/com/kumar/tasks/services/impl/TaskServiceImpl.java
@@ -1,24 +1,70 @@
 package com.kumar.tasks.services.impl;
 
 import com.kumar.tasks.domain.entities.Task;
+import com.kumar.tasks.domain.entities.TaskList;
+import com.kumar.tasks.domain.entities.TaskPriority;
+import com.kumar.tasks.domain.entities.TaskStatus;
+import com.kumar.tasks.repositories.TaskListRepository;
 import com.kumar.tasks.repositories.TaskRepository;
 import com.kumar.tasks.services.TaskService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class TaskServiceImpl implements TaskService {
 
     private final TaskRepository taskRepository;
+    private final TaskListRepository taskListRepository;
 
-    public TaskServiceImpl(TaskRepository taskRepository) {
+    public TaskServiceImpl(TaskRepository taskRepository, TaskListRepository taskListRepository) {
         this.taskRepository = taskRepository;
+        this.taskListRepository = taskListRepository;
     }
 
     @Override
     public List<Task> listTasks(UUID taskListId) {
         return taskRepository.findByTaskListId(taskListId);
+    }
+
+    @Override
+    public Task createTask(UUID taskListId, Task task) {
+        if(null != task.getId()){
+            throw new IllegalArgumentException("Task already has an ID!");
+        }
+
+        if(null == task.getTitle() || task.getTitle().isBlank()){
+            throw new IllegalArgumentException("Task must have a title!");
+        }
+
+        TaskPriority taskPriority = Optional.ofNullable(task.getPriority()).
+                orElse(TaskPriority.MEDIUM);
+
+        TaskStatus taskStatus = TaskStatus.OPEN;
+
+        TaskList taskList = taskListRepository.findById(taskListId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid Task List ID provided"));
+
+        LocalDateTime now = LocalDateTime.now();
+
+        Task taskToSave = new Task(
+                null,
+                task.getTitle(),
+                task.getDescription(),
+                task.getDueDate(),
+                taskStatus,
+                taskPriority,
+                taskList,
+                now,
+                now
+        );
+
+        return taskRepository.save(taskToSave);
+
+
+
     }
 }
