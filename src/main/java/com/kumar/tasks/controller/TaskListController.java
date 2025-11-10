39098d00b86780fccc4feb3effaf305605f88520
unknown <kumardeepsingh@student.kpu.ca>
2025-11-09 16:27:15 -0800

    Added get TaskListByID endpoint

--- src/main/java/com/kumar/tasks/controller/TaskListController.java
+++ src/main/java/com/kumar/tasks/controller/TaskListController.java
@@ -7,6 +7,8 @@ import com.kumar.tasks.services.TaskListService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/api/task-lists")
@@ -34,5 +36,10 @@ public class TaskListController {
                 taskListMapper.fromDto(taskListDto)
         );
         return taskListMapper.toDto(createdTaskList);
+    }
+
+    @GetMapping(path = "/{task_list_id}")
+    public Optional<TaskListDto> getTaskLlist(@PathVariable("task_list_id") UUID taskListId){
+        return taskListService.getTaskList(taskListId).map(taskListMapper::toDto);
     }
 }
--- src/main/java/com/kumar/tasks/services/TaskListService.java
+++ src/main/java/com/kumar/tasks/services/TaskListService.java
@@ -3,9 +3,12 @@ package com.kumar.tasks.services;
 import com.kumar.tasks.domain.entities.TaskList;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface TaskListService {
     List<TaskList> listTaskLists();
     TaskList createTaskList(TaskList taskList);
+    Optional<TaskList> getTaskList(UUID id);
 
 }
