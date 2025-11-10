b6fe9f701f5aae4d2248bdf2aef62e779878e993
unknown <kumardeepsingh@student.kpu.ca>
2025-11-09 17:52:47 -0800

    Added get task by taskListId and taskId endpoint

--- src/main/java/com/kumar/tasks/services/TaskService.java
+++ src/main/java/com/kumar/tasks/services/TaskService.java
@@ -3,9 +3,11 @@ package com.kumar.tasks.services;
 import com.kumar.tasks.domain.entities.Task;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TaskService {
     List<Task> listTasks(UUID taskListId);
     Task createTask(UUID taskListId, Task task);
+    Optional<Task> getTask(UUID taskListId, UUID taskId);
 }
